8834bcc986a693b92b6bac09db1f6e2619cfcebc
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2017-05-21 13:17:46 +0200

    Convert mockup to Elm.

--- Main.elm
+++ Main.elm
@@ -1,3 +1,14 @@
-import Html
+import Html exposing (Html, div, img, text)
+import Html.Attributes exposing (src, style)
 
-main = Html.text "Hello World"
+main: Html msg
+main =
+  div [style [("position", "relative"), ("margin", "50px 0px 0px 50px")]] [
+    img [src "empty-board.png"] [],
+    div [style [("border", "2px blue solid"), ("width", "49px"), ("height", "49px"), ("position", "absolute"), ("left", "106px"), ("top", "106px")]] [],
+    img [src "knight.png", style [("position", "absolute"), ("left", "106px"), ("top", "106px")]] [],
+    div [style [("border", "2px purple solid"), ("width", "49px"), ("height", "49px"), ("position", "absolute"), ("left", "265px"), ("top", "212px")]] [],
+    div [style [("text-align", "center"), ("font-size", "46px"), ("width", "53px"), ("height", "53px"), ("position", "absolute"), ("left", "265px"), ("top", "212px")]] [text "1"],
+    div [style [("border", "2px purple solid"), ("width", "49px"), ("height", "49px"), ("position", "absolute"), ("left", "318px"), ("top", "318px")]] [],
+    div [style [("text-align", "center"), ("font-size", "46px"), ("width", "53px"), ("height", "53px"), ("position", "absolute"), ("left", "318px"), ("top", "318px")]] [text "2"]
+  ]
